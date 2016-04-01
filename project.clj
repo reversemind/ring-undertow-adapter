fe4954527286fc01f8a68482baa64762da070671
Eugene Kalinin <kalinin.eugene@gmail.com>
2016-04-02 00:36:48 +0300

    Upgraded versions

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject ring-undertow-adapter "0.2.2"
+(defproject ring-undertow-adapter "0.2.3"
   :description "Ring Underow adapter."
   :url "http://github.com/piranha/ring-adapter-undertow"
   :license {:name "ISC License"
             :url "http://opensource.org/licenses/ISC"}
-  :dependencies [[io.undertow/undertow-core "1.1.0.Final"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [clj-http "1.0.1"]]}})
+  :dependencies [[io.undertow/undertow-core "1.3.20.Final"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+                                  [clj-http "2.1.0"]]}})
